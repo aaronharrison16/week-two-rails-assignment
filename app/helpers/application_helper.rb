1dfbaa3325e60d4760fef3d7c4beba6f163275e1
Aaron Harrison <aaronharrison16@gmail.com>
2018-05-20 10:47:22 -0600

    Completed requirements for project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def login_helper
+    if admin_signed_in?
+      link_to "Logout", destroy_admin_session_path, method: :delete
+    else
+      (link_to "Sign In", new_admin_session_path) + " " +
+      (link_to "Register",new_admin_registration_path)
+    end
+  end
 end
